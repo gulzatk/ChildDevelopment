cd1d1971bca0903fa50fbf587d348496ec2e9c04
Stuart Gill <stuart.a.gill@gmail.com>
2018-12-20 11:20:21 -0800

    updates child controller

--- ChildDevelopment/Controllers/ChildrenController.cs
+++ ChildDevelopment/Controllers/ChildrenController.cs
@@ -1,66 +1,66 @@
-// using System.Collections.Generic;
-// using Microsoft.AspNetCore.Mvc;
-// using ChildDevelopment.Models;
-// using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ChildDevelopment.Models;
+using System;
 
-// namespace ChildDevelopment.Controllers
-// {
-//     public class ChildrenController : Controller
-//     {
-//         [HttpGet("/children")]
-//         public ActionResult Index()
-//         {
-//             List<Child> children = Child.GetAll();
-//             return View(children);
-//         }
+namespace ChildDevelopment.Controllers
+{
+    public class ChildrenController : Controller
+    {
+        [HttpGet("/children")]
+        public ActionResult Index()
+        {
+            List<Child> children = Child.GetAll();
+            return View(children);
+        }
 
-//         [HttpGet("/children/new")]
-//         public ActionResult New()
-//         {
-//             return View();
-//         }
+        [HttpGet("/children/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
 
 
-//         [HttpPost("/children")]
-//         public ActionResult Index(
-//         string name,
-//         bool gender,
-//         int weight,
-//         int height,
-//         DateTime birth_date,
-//         bool breastfeeding,
-//         DateTime hold_head,
-//         DateTime roll_over,
-//         DateTime first_teeth,
-//         DateTime sit,
-//         DateTime crawl,
-//         DateTime walk,
-//         DateTime first_words,
-//         DateTime self_feeding,
-//         DateTime make_believe,
-//         DateTime two_word_sentence,
-//         DateTime potty_trained,
-//         DateTime dressed_self,
-//         DateTime tell_story,
-//         DateTime read_write
-//         )
-//         {
-//             DateTime[] events= new DateTime[]{hold_head,
-//             roll_over,
-//             first_teeth,
-//             sit,
-//             crawl,
-//             walk,
-//             first_words,
-//             self_feeding,
-//             make_believe,
-//             two_word_sentence,
-//             potty_trained,
-//             dressed_self,
-//             tell_story,
-//             read_write};
-//             Child child = new Child(name,gender,weight,height,birth_date,breastfeeding);
-//             child.Save();
+        [HttpPost("/children")]
+        public ActionResult Index(
+        string name,
+        bool gender,
+        int weight,
+        int height,
+        DateTime birth_date,
+        bool breastfeeding,
+        DateTime hold_head,
+        DateTime roll_over,
+        DateTime first_teeth,
+        DateTime sit,
+        DateTime crawl,
+        DateTime walk,
+        DateTime first_words,
+        DateTime self_feeding,
+        DateTime make_believe,
+        DateTime two_word_sentence,
+        DateTime potty_trained,
+        DateTime dressed_self,
+        DateTime tell_story,
+        DateTime read_write
+        )
+        {
+            DateTime[] events= new DateTime[]{hold_head,
+            roll_over,
+            first_teeth,
+            sit,
+            crawl,
+            walk,
+            first_words,
+            self_feeding,
+            make_believe,
+            two_word_sentence,
+            potty_trained,
+            dressed_self,
+            tell_story,
+            read_write};
+            Child child = new Child(name,gender,weight,height,birth_date,breastfeeding);
+            child.Save();
 
             int childId = child.GetId();
             for (int i=1;i<=14;i++)
@@ -84,8 +84,8 @@
             }
             result += "]";
 
-//             return View("Index",result);
-//         }
+            return View("Index",result);
+        }
 
-//     }
-// }
+    }
+}
